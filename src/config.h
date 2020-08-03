7876f35e39e02fe362c5b15293998749f28f8ee5
sharandac <sharandac@snafu.de>
2020-08-04 01:33:41 +0200

    update sound

--- src/config.h
+++ src/config.h
@@ -31,6 +31,6 @@
     /*
     * firmeware version string
     */
-    #define __FIRMWARE__            "2020080404"
+    #define __FIRMWARE__            "2020080405"
 
 #endif // _CONFIG_H
--- src/hardware/sound.cpp
+++ src/hardware/sound.cpp
@@ -38,7 +38,7 @@ void sound_setup( void ) {
         .sample_rate = 8000,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_PCM | I2S_COMM_FORMAT_I2S_MSB),
+        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
         .dma_buf_count = 8,
         .dma_buf_len = 64   //Interrupt level 1
