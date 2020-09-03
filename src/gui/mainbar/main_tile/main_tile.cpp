14596444772f835a76ecc8cad9afb26b133bb7bd
Stephen Denne <stephen@datacute.co.nz>
2020-09-03 13:46:39 +1200

    Stop showing leading zero when not 24h

--- src/gui/mainbar/main_tile/main_tile.cpp
+++ src/gui/mainbar/main_tile/main_tile.cpp
@@ -47,6 +47,7 @@ lv_task_t * main_tile_task;
 
 void main_tile_update_task( lv_task_t * task );
 void main_tile_align_widgets( void );
+void main_tile_format_time( char *, size_t, struct tm * );
 
 void main_tile_setup( void ) {
     main_tile_num = mainbar_add_tile( 0, 0 );
@@ -76,14 +77,10 @@ void main_tile_setup( void ) {
     lv_obj_add_style( datelabel, LV_OBJ_PART_MAIN, &datestyle );
     lv_obj_align( datelabel, clock_cont, LV_ALIGN_IN_BOTTOM_MID, 0, 0 );
 
-    time_t now;
     struct tm  info;
     char buf[64];
 
-    time( &now );
-    localtime_r( &now, &info );
-
-    strftime( buf, sizeof(buf), "%H:%M", &info );
+    main_tile_format_time( buf, sizeof(buf), &info );
     lv_label_set_text( timelabel, buf );
     strftime( buf, sizeof(buf), "%a %d.%b %Y", &info );
     lv_label_set_text( datelabel, buf );
@@ -170,11 +167,9 @@ uint32_t main_tile_get_tile_num( void ) {
 }
 
 void main_tile_update_task( lv_task_t * task ) {
-    time_t now;
     struct tm  info;
     char time_str[64]="";
     static char *old_time_str = NULL;
-    const char * time_fmt = timesync_get_24hr() ? "%H:%M" : "%I:%M";
 
     // on first run, alloc psram
     if ( old_time_str == NULL ) {
@@ -185,9 +180,7 @@ void main_tile_update_task( lv_task_t * task ) {
         }
     }
 
-    time( &now );
-    localtime_r( &now, &info );
-    strftime( time_str, sizeof(time_str), time_fmt, &info );
+    main_tile_format_time( time_str, sizeof(time_str), &info );
 
     // only update while time_str changes
     if ( strcmp( time_str, old_time_str ) ) {
@@ -200,4 +193,19 @@ void main_tile_update_task( lv_task_t * task ) {
         lv_label_set_text( datelabel, time_str );
         lv_obj_align( datelabel, clock_cont, LV_ALIGN_IN_BOTTOM_MID, 0, 0 );
     }
+}
+
+void main_tile_format_time( char * buf, size_t buf_len, struct tm * info ) {
+    time_t now;
+    const char * time_fmt = "%2d:%02d";
+
+    time( &now );
+    localtime_r( &now, info );
+    int h = info->tm_hour;
+    int m = info->tm_min;
+    if (!timesync_get_24hr()) {
+        if (h == 0) h = 12;
+        if (h > 12) h -= 12;
+    }
+    snprintf( buf, buf_len, time_fmt, h, m );
 }
