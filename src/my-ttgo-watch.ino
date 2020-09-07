7720195598664e0b0605995500607fa1493176ef
rnisthal <roberto@telemensaje.com.gt>
2020-09-07 13:18:47 -0600

    panico en true en el watchdog

--- src/my-ttgo-watch.ino
+++ src/my-ttgo-watch.ino
@@ -24,6 +24,7 @@
 #include "config.h"
 #include <Arduino.h>
 #include "esp_bt.h"
+#include "esp_task_wdt.h"
 
 #include "gui/gui.h"
 #include "gui/splashscreen.h"
@@ -50,7 +51,7 @@ void setup()
 {
     Serial.begin(115200);
     Serial.printf("starting t-watch V1, version: " __FIRMWARE__ "\r\n");
-    Serial.printf("Configure watchdog to 30s: %d\r\n", esp_task_wdt_init(30, false));
+    Serial.printf("Configure watchdog to 30s: %d\r\n", esp_task_wdt_init(30, true));
     
     ttgo->begin();
     ttgo->lvgl_begin();
