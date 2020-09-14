a184fe70d400d19a18b2f1d8b2d8097d4b019162
Dirk Broßwick <dirk.brosswick@googlemail.com>
2020-09-14 16:38:33 +0200

    Update display.h

--- src/hardware/display.h
+++ src/hardware/display.h
@@ -42,92 +42,91 @@
     #define DISPLAY_CONFIG_FILE         "/display.cfg"
     #define DISPLAY_JSON_CONFIG_FILE    "/display.json"
 
-    /*
+    /**
      * @brief setup display
      * 
      * @param   ttgo    pointer to an TTGOClass
      */
     void display_setup( void );
-    /*
+    /**
      * @brief display loop
      * 
      * @param   ttgo    pointer to an TTGOClass
      */
     void display_loop( void );
-    /*
+    /**
      * @brief save config for display to spiffs
      */
     void display_save_config( void );
-    /*
+    /**
      * @brief read config for display from spiffs
      */
     void display_read_config( void );
-    /*
+    /**
      * @brief read the timeout from config
      * 
      * @return  timeout in seconds
      */
     uint32_t display_get_timeout( void );
-    /*
+    /**
      * @brief set timeout for the display
      * 
      * @param timeout in seconds
      */
     void display_set_timeout( uint32_t timeout );
-    /*
+    /**
      * @brief read the brightness from config
      * 
      * @return  brightness from 0-255
      */
     uint32_t display_get_brightness( void );
-    /*
+    /**
      * @brief set brightness for the display
      * 
      * @param brightness brightness from 0-255
      */
     void display_set_brightness( uint32_t brightness );
-    /*
+    /**
      * @brief read the rotate from the display
      * 
      * @return  rotation from 0-270 degree in 90 degree steps
      */
     uint32_t display_get_rotation( void );
-    /*
+    /**
      * @brief set the rotate for the display
      * 
      * @param rotation from 0-270 in 90 degree steps
      */
     void display_set_rotation( uint32_t rotation );
-    /*
+    /**
      * @brief read the block_return_maintile while wakeup
      * 
      * @param rotation from 0-270 in 90 degree steps
      */
     bool display_get_block_return_maintile( void );
-    /*
+    /**
      * @brief set the block_return_maintile while wakeup
      * 
      * @param block_return_maintile true or false, true means no autoreturn zu maintile
      */
     void display_set_block_return_maintile( bool block_return_maintile );
-    /*
-     * @brief set display into standby
+    /**
+     * @brief get the background for the display
+     * 
+     * return image number
      */
     uint32_t display_get_background_image( void );
-    /*
+    /**
      * @brief set the background for the display
      * 
-     * @param image number
+     * @param background_image image number
      */
     void display_set_background_image( uint32_t background_image );
-    /*
-     * @brief read the block_return_maintile while wakeup
-     * 
-     * @param image number
+    /**
+     * @brief set display into standby
      */
-
     void display_standby( void );
-    /*
+    /**
      * @brief set display into normal mode or leave it in standby if a silence wakeup occur
      */
     void display_wakeup( bool silence );
