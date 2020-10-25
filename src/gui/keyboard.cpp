0dc84ff012d24cde337d75535f5ec4916656507a
sharandac <sharandac@snafu.de>
2020-10-25 10:24:04 +0100

    update binary files

--- src/gui/keyboard.cpp
+++ src/gui/keyboard.cpp
@@ -35,26 +35,26 @@ static bool kb_style_initialized = false;
 static void kb_event_cb(lv_obj_t * ta, lv_event_t event);
 
 void keyboard_prelim( void ) {
-    if(!kb_style_initialized) {
-	lv_style_init( &kb_style );
-	lv_style_set_radius(&kb_style, LV_OBJ_PART_MAIN, 0);
-	lv_style_set_bg_color(&kb_style, LV_OBJ_PART_MAIN, LV_COLOR_GRAY);
-	lv_style_set_bg_opa(&kb_style, LV_OBJ_PART_MAIN, LV_OPA_100);
-	lv_style_set_border_width(&kb_style, LV_OBJ_PART_MAIN, 0);
-	lv_style_set_text_color(&kb_style, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
-	lv_style_set_image_recolor(&kb_style, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
-	    
-	kb_screen = lv_cont_create( lv_scr_act(), NULL );
-	lv_obj_add_style( kb_screen, LV_OBJ_PART_MAIN, &kb_style );
-	lv_obj_set_size( kb_screen, lv_disp_get_hor_res( NULL ) , lv_disp_get_ver_res( NULL ) - 20 );
-	lv_obj_align( kb_screen, lv_scr_act(), LV_ALIGN_CENTER, 0, 20);
-	
-	kb_textarea = lv_textarea_create( kb_screen, NULL );
-	lv_obj_add_protect( kb_textarea, LV_PROTECT_CLICK_FOCUS);
-	lv_obj_set_size( kb_textarea, lv_disp_get_hor_res( NULL ) - 10, 60 );
-	lv_textarea_set_one_line( kb_textarea, true);
-	lv_obj_align( kb_textarea, kb_screen, LV_ALIGN_IN_TOP_MID, 0, 5 );
-	kb_style_initialized = true;
+    if( !kb_style_initialized ) {
+        lv_style_init( &kb_style );
+        lv_style_set_radius( &kb_style, LV_OBJ_PART_MAIN, 0);
+        lv_style_set_bg_color( &kb_style, LV_OBJ_PART_MAIN, LV_COLOR_GRAY);
+        lv_style_set_bg_opa( &kb_style, LV_OBJ_PART_MAIN, LV_OPA_100);
+        lv_style_set_border_width( &kb_style, LV_OBJ_PART_MAIN, 0);
+        lv_style_set_text_color( &kb_style, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
+        lv_style_set_image_recolor( &kb_style, LV_OBJ_PART_MAIN, LV_COLOR_WHITE);
+            
+        kb_screen = lv_cont_create( lv_scr_act(), NULL );
+        lv_obj_add_style( kb_screen, LV_OBJ_PART_MAIN, &kb_style );
+        lv_obj_set_size( kb_screen, lv_disp_get_hor_res( NULL ) , lv_disp_get_ver_res( NULL ) - 20 );
+        lv_obj_align( kb_screen, lv_scr_act(), LV_ALIGN_CENTER, 0, 20);
+        
+        kb_textarea = lv_textarea_create( kb_screen, NULL );
+        lv_obj_add_protect( kb_textarea, LV_PROTECT_CLICK_FOCUS);
+        lv_obj_set_size( kb_textarea, lv_disp_get_hor_res( NULL ) - 10, 60 );
+        lv_textarea_set_one_line( kb_textarea, true);
+        lv_obj_align( kb_textarea, kb_screen, LV_ALIGN_IN_TOP_MID, 0, 5 );
+        kb_style_initialized = true;
     }
 }
 
@@ -63,6 +63,7 @@ void keyboard_setup( void ) {
         return;
 
     keyboard_prelim();
+
     kb = lv_keyboard_create( lv_scr_act() , NULL);
     lv_obj_set_size (kb, lv_disp_get_hor_res( NULL ), ( lv_disp_get_ver_res( NULL ) / 4 ) * 3 - 20);
     lv_obj_align( kb, lv_scr_act(), LV_ALIGN_IN_BOTTOM_MID, 0, 0 );
@@ -118,22 +119,26 @@ void num_keyboard_set_textarea( lv_obj_t *textarea ){
 
 void keyboard_hide( void ) {
     if ( kb_screen != NULL ) {
-	lv_obj_set_hidden( kb_screen, true );
+    	lv_obj_set_hidden( kb_screen, true );
     }
+
     if ( kb_textarea != NULL) {
-	lv_obj_set_hidden( kb_textarea, true );
+    	lv_obj_set_hidden( kb_textarea, true );
     }
+
     if( kb != NULL ) {
-	lv_obj_set_hidden( kb, true );
+    	lv_obj_set_hidden( kb, true );
     }
+
     if( nkb != NULL ) {
-	lv_obj_set_hidden( nkb, true );
+    	lv_obj_set_hidden( nkb, true );
     }
 }
 
 void keyboard_show( void ) {
     if ( kb == NULL )
         return;
+
     lv_obj_set_hidden( kb_screen, false );
     lv_obj_set_hidden( kb_textarea, false );
     lv_obj_set_hidden( kb, false );
