70893c99272da64abc369391a7bb7e82193c6e0d
anakod <me@anakod.ru>
2020-12-01 20:29:04 +0500

    Different character sets support
    By default used standard latin charset as it was before. To enable this feature please define USE_EXTENDED_CHARSET in config.h

---
+++ src/gui/charsets.h
@@ -0,0 +1,9 @@
+// Available constants for USE_EXTENDED_CHARSET
+
+#ifndef _GUI_CHARSETS_H
+    #define _GUI_CHARSETS_H
+
+    #define CHARSET_CYRILLIC 1251
+    //TODO: There's more charsets to come
+
+#endif
