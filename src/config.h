bfff01918b08e0908a57af63dc2cc290db8e185f
sharandac <sharandac@snafu.de>
2021-01-13 09:21:35 +0100

    test custom arduino-esp32 framework

--- src/config.h
+++ src/config.h
@@ -25,8 +25,8 @@
 
     #define LILYGO_WATCH_2020_V1             //To use T-Watch2020, please uncomment this line
     #define LILYGO_WATCH_LVGL                //To use LVGL, you need to enable the macro LVGL
-    #define TWATCH_USE_PSRAM_ALLOC_LVGL
-//    #define LVGL_BUFFER_SIZE        (240*25)
+//    #define TWATCH_USE_PSRAM_ALLOC_LVGL
+    #define LVGL_BUFFER_SIZE        (240*25)
 
     /*
     * Built-in applications
