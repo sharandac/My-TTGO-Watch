404fbca9ab893c6cc5a08d930837e5982c5ef7ff
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2021-02-11 22:49:43 +0100

    Switch BLE config to BaseJsonConfig
    Reducing similar code helps maintainability.

---
+++ src/hardware/blectlconfig.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+ *   Aug 11 17:13:51 2020
+ *   Copyright  2020  Dirk Brosswick
+ *   Email: dirk.brosswick@googlemail.com
+ ****************************************************************************/
+ 
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifndef _BLECTL_CONFIG_H
+    #define _BLECTL_CONFIG_H
+
+    #include "basejsonconfig.h"
+
+    #define BLECTL_JSON_COFIG_FILE         "/blectl.json"   /** @brief defines json config file name */
+
+    /**
+     * @brief blectl config structure
+     */
+    class blectl_config_t : public BaseJsonConfig {
+        public:
+        blectl_config_t();
+        bool autoon = true;             /** @brief auto on/off */
+        bool advertising = true;        /** @brief advertising on/off */
+        bool enable_on_standby = false; /** @brief enable on standby on/off */
+        bool show_notification = true;  /** @brief enable show notifications */
+        int32_t txpower = 1;            /** @brief tx power, valide values are from 0 to 4 */
+
+        protected:
+        ////////////// Available for overloading: //////////////
+        virtual bool onLoad(JsonDocument& document);
+        virtual bool onSave(JsonDocument& document);
+        virtual size_t getJsonBufferSize() { return 1000/*FIXME*/; }
+    } ;
+
+#endif // _BLECTL_CONFIG_H
