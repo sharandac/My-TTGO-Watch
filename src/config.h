e23b77aa1816039425c8c5870995691f83c139ea
sharandac <sharandac@snafu.de>
2021-03-01 13:09:52 +0100

    preparation for a new t-watch-2020 version

--- src/config.h
+++ src/config.h
@@ -23,7 +23,6 @@
  */
 #ifndef _CONFIG_H 
 
-    #define LILYGO_WATCH_2020_V1             //To use T-Watch2020, please uncomment this line
     #define LILYGO_WATCH_LVGL                //To use LVGL, you need to enable the macro LVGL
     #define TWATCH_USE_PSRAM_ALLOC_LVGL
 
