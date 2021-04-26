f07acafe0fb6dc3cfca365cd1fa9e9f13445f723
sharandac <sharandac@snafu.de>
2021-04-26 15:38:26 +0200

    update doku

--- src/config.h
+++ src/config.h
@@ -48,7 +48,7 @@
     /**
      * firmeware version string
      */
-    #define __FIRMWARE__            "2021042601"
+    #define __FIRMWARE__            "2021042602"
     /**
      * Allows to include config.h from C code
      */
