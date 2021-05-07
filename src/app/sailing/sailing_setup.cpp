5f37e091e143b6065813355cc977fabafbd7a62e
sharandac <sharandac@snafu.de>
2021-05-07 17:44:35 +0200

    update app to wf

--- src/app/sailing/sailing_setup.cpp
+++ src/app/sailing/sailing_setup.cpp
@@ -45,41 +45,33 @@ static void sailing_foobar_switch_event_cb( lv_obj_t * obj, lv_event_t event );
 static void sailing_track_switch_event_cb( lv_obj_t * obj, lv_event_t event );
 
 void sailing_setup_setup( uint32_t tile_num ) {
-
+    /**
+     * get tile obj from tile number
+     */
     sailing_setup_tile = mainbar_get_tile_obj( tile_num );
+    /**
+     * get mainbar style, setup it and add it to setup tile
+     */
     lv_style_copy( &sailing_setup_style, ws_get_mainbar_style() );
-
     lv_style_set_bg_color( &sailing_setup_style, LV_OBJ_PART_MAIN, LV_COLOR_GRAY);
     lv_style_set_bg_opa( &sailing_setup_style, LV_OBJ_PART_MAIN, LV_OPA_100);
     lv_style_set_border_width( &sailing_setup_style, LV_OBJ_PART_MAIN, 0);
     lv_obj_add_style( sailing_setup_tile, LV_OBJ_PART_MAIN, &sailing_setup_style );
-
+    /**
+     * add setup header with exit button
+     */
     lv_obj_t *header = wf_add_settings_header( sailing_setup_tile, "Exit setup", exit_sailing_setup_event_cb );
     lv_obj_align( header, sailing_setup_tile, LV_ALIGN_IN_TOP_LEFT, 10, 10 );
-
-    lv_obj_t *sailing_foobar_switch_cont = lv_obj_create( sailing_setup_tile, NULL );
-    lv_obj_set_size( sailing_foobar_switch_cont, lv_disp_get_hor_res( NULL ) , 40);
-    lv_obj_add_style( sailing_foobar_switch_cont, LV_OBJ_PART_MAIN, &sailing_setup_style  );
-    lv_obj_align( sailing_foobar_switch_cont, header, LV_ALIGN_OUT_BOTTOM_MID, 0, 0 );
-    sailing_foobar_switch = wf_add_switch( sailing_foobar_switch_cont, false );
-    lv_obj_align( sailing_foobar_switch, sailing_foobar_switch_cont, LV_ALIGN_IN_RIGHT_MID, -5, 0 );
-    lv_obj_set_event_cb( sailing_foobar_switch, sailing_foobar_switch_event_cb );
-    lv_obj_t *sailing_foobar_switch_label = lv_label_create( sailing_foobar_switch_cont, NULL);
-    lv_obj_add_style( sailing_foobar_switch_label, LV_OBJ_PART_MAIN, &sailing_setup_style  );
-    lv_label_set_text( sailing_foobar_switch_label, "Always on display");
-    lv_obj_align( sailing_foobar_switch_label, sailing_foobar_switch_cont, LV_ALIGN_IN_LEFT_MID, 5, 0 );
-
-    lv_obj_t *sailing_track_switch_cont = lv_obj_create( sailing_setup_tile, NULL );
-    lv_obj_set_size( sailing_track_switch_cont, lv_disp_get_hor_res( NULL ) , 40);
-    lv_obj_add_style( sailing_track_switch_cont, LV_OBJ_PART_MAIN, &sailing_setup_style  );
-    lv_obj_align( sailing_track_switch_cont, sailing_foobar_switch_cont, LV_ALIGN_OUT_BOTTOM_MID, 0, 0 );
-    sailing_track_switch = wf_add_switch( sailing_track_switch_cont, false );
-    lv_obj_align( sailing_track_switch, sailing_track_switch_cont, LV_ALIGN_IN_RIGHT_MID, -5, 0 );
-    lv_obj_set_event_cb( sailing_track_switch, sailing_track_switch_event_cb );
-    lv_obj_t *sailing_track_switch_label = lv_label_create( sailing_track_switch_cont, NULL);
-    lv_obj_add_style( sailing_track_switch_label, LV_OBJ_PART_MAIN, &sailing_setup_style  );
-    lv_label_set_text( sailing_track_switch_label, "Show track");
-    lv_obj_align( sailing_track_switch_label, sailing_track_switch_cont, LV_ALIGN_IN_LEFT_MID, 5, 0 );
+    /**
+     * setup always on display switch
+     */
+    lv_obj_t *sailing_foobar_switch_cont = wf_add_labeled_switch( sailing_setup_tile, "Always on display", &sailing_foobar_switch, false, sailing_foobar_switch_event_cb );
+    lv_obj_align( sailing_foobar_switch_cont, header, LV_ALIGN_OUT_BOTTOM_MID, 0, 5 );
+    /**
+     * setup show track switch
+     */
+    lv_obj_t *sailing_track_switch_cont = wf_add_labeled_switch( sailing_setup_tile, "Show track", &sailing_track_switch, false, sailing_track_switch_event_cb );
+    lv_obj_align( sailing_track_switch_cont, sailing_foobar_switch_cont, LV_ALIGN_OUT_BOTTOM_MID, 0, 5 );
 }
 
 static void sailing_foobar_switch_event_cb( lv_obj_t * obj, lv_event_t event ) {
