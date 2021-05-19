82b099d4b6f9ff6d86c662e4fe9b85df0beed9d6
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2021-05-19 22:37:19 +0200

    Design a full screen app

--- src/app/calendar/calendar.cpp
+++ src/app/calendar/calendar.cpp
@@ -1,20 +1,41 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 #include "config.h"
 #include <TTGO.h>
-#include "quickglui/quickglui.h"
 
 #include "calendar.h"
 #include "gui/mainbar/mainbar.h"
-#include "gui/widget_styles.h"
-#include "hardware/motor.h"
+#include "gui/icon.h"
+#include "gui/statusbar.h"
+#include "gui/app.h"
+
+uint32_t calendar_main_tile_num;
+
+// app icon
+icon_t *calendar_icon = NULL;
 
 // App icon must have an size of 64x64 pixel with an alpha channel
 // Use https://lvgl.io/tools/imageconverter to convert your images and set "true color with alpha"
 LV_IMG_DECLARE(calendar_64px);
 
-static Application calendarApp;
 
 static lv_obj_t * calendar;
 
+static void enter_calendar_event_cb( lv_obj_t * obj, lv_event_t event );
+static void event_handler(lv_obj_t * obj, lv_event_t event);
 static void build_main_page();
 static void refresh_main_page();
 
@@ -23,18 +44,32 @@ static void calendar_activate_cb();
 /*
  * setup routine for application
  */
-void calendar_app_setup() {
-    // Create and register new application
-    //   params: name, icon, auto add "refresh" button (this app will use synchronize function of the SynchronizedApplication class).
-    //   Also, you can configure count of the required pages in the next two params (to have more app screens).
-    calendarApp.init("calendar", &calendar_64px, 1, 1);
+void calendar_app_setup( void ) {
+    calendar_main_tile_num = mainbar_add_app_tile( 1, 1, "Calendar" );
+    mainbar_add_tile_activate_cb( calendar_main_tile_num, calendar_activate_cb );
 
-    mainbar_add_tile_activate_cb( calendarApp.mainTileId(), calendar_activate_cb );
+    // register app icon on the app tile
+    // set your own icon and register her callback to activate by an click
+    // remember, an app icon must have an size of 64x64 pixel with an alpha channel
+    // use https://lvgl.io/tools/imageconverter to convert your images and set "true color with alpha" to get fancy images
+    // the resulting c-file can put in /app/examples/images/ and declare it like LV_IMG_DECLARE( your_icon );
+    calendar_icon = app_register( "Calendar", &calendar_64px, enter_calendar_event_cb );
+    app_set_indicator( calendar_icon, ICON_INDICATOR_OK );
 
     // Build and configure application
     build_main_page();
 
     refresh_main_page();
+}
+
+static void enter_calendar_event_cb( lv_obj_t * obj, lv_event_t event ) {
+    switch( event ) {
+        case( LV_EVENT_CLICKED ):
+               statusbar_hide( true );
+               app_hide_indicator( calendar_icon );
+               mainbar_jump_to_tilenumber( calendar_main_tile_num, LV_ANIM_OFF );
+               break;
+    }
 }
 
 static void event_handler(lv_obj_t * obj, lv_event_t event)
@@ -43,13 +78,14 @@ static void event_handler(lv_obj_t * obj, lv_event_t event)
         mainbar_jump_back();
     }
 }
+
 void build_main_page()
 {
-    AppPage& screen = calendarApp.mainPage(); // This is parent for all main screen widgets
+    lv_obj_t *main_tile = mainbar_get_tile_obj( calendar_main_tile_num );
 
-    calendar = lv_calendar_create(screen.handle(), NULL);
+    calendar = lv_calendar_create(main_tile, NULL);
     lv_obj_set_size(calendar, LV_HOR_RES, LV_VER_RES);
-    lv_obj_align(calendar, screen.handle(), LV_ALIGN_IN_TOP_MID, 0, 0);
+    lv_obj_align(calendar, main_tile, LV_ALIGN_IN_TOP_MID, 0, 0);
     lv_obj_set_event_cb(calendar, event_handler);
 
     /*Make the date number smaller to be sure they fit into their area*/
