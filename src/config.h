d4b7953d1811fcb349953ac3a3d8d8dc2e1bf0b4
sharandac <sharandac@snafu.de>
2021-05-25 18:57:52 +0200

    update version

--- src/config.h
+++ src/config.h
@@ -48,7 +48,7 @@
     /**
      * firmeware version string
      */
-    #define __FIRMWARE__            "2021052501"
+    #define __FIRMWARE__            "2021052502"
     /**
      * Allows to include config.h from C code
      */
