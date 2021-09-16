74a9b00c998fc95774e38f64688e8659d188fabf
sharandac <sharandac@snafu.de>
2021-09-16 16:04:01 +0200

    fix segfault in weather fetch

--- src/utils/filepath_convert.cpp
+++ src/utils/filepath_convert.cpp
@@ -6,6 +6,7 @@
     #include <string.h>
     #include <unistd.h>
     #include <sys/types.h>
+    #include <sys/stat.h>
     #include <pwd.h>
     #include "utils/logging.h"
 #else
@@ -19,6 +20,26 @@
 char *filepath_convert( char * dst_str, int max_len, const char* local_path ) {
 
     #ifdef NATIVE_64BIT
+        char hedge_config_path[512] = "";
+        struct stat st = {0};
+
+        /**
+         * check config path
+         */
+        if ( getenv("HOME") )
+            snprintf( hedge_config_path, sizeof( hedge_config_path ), "%s/.hedge", getpwuid(getuid())->pw_dir );
+        /**
+         * create config dir if not exist
+         */
+        if ( stat( hedge_config_path, &st ) == -1) {
+            log_i("create config path and dir");
+            mkdir( hedge_config_path, 0700 );
+            snprintf( hedge_config_path, sizeof( hedge_config_path ), "%s/.hedge/spiffs", getpwuid(getuid())->pw_dir );
+            mkdir( hedge_config_path, 0700 );
+            snprintf( hedge_config_path, sizeof( hedge_config_path ), "%s/.hedge/sd", getpwuid(getuid())->pw_dir );
+            mkdir( hedge_config_path, 0700 );
+        }
+
         if ( getenv("HOME") )
             snprintf( dst_str, max_len, "%s/.hedge/%s", getpwuid(getuid())->pw_dir, local_path );
     #else
