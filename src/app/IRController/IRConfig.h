03454f7e272f8fab366c81ae54a97110f02a7553
Dirk Sarodnick <github@dirk-sarodnick.de>
2021-12-17 12:48:00 +0100

    added support for multiple commands to IR; refactored IR button array allocation to be more dynamic

--- src/app/IRController/IRConfig.h
+++ src/app/IRController/IRConfig.h
@@ -10,38 +10,39 @@
         #if defined( M5PAPER ) || defined( M5CORE2 ) || defined( LILYGO_WATCH_2021 )  
 
         #elif defined( LILYGO_WATCH_2020_V1 ) || defined( LILYGO_WATCH_2020_V2 ) || defined( LILYGO_WATCH_2020_V3 )
-            #define MAX_IR_BUTTONS 48
+            #define IR_BUTTONS_START 16
+            #define IR_BUTTONS_TRESHOLD 8
 
             class IRConfig : public BaseJsonConfig
             {
-            public:
-            IRConfig();
+                public:
+                IRConfig();
 
-            InfraButton* add(size_t page, const char* name);
-            void del(size_t page, const char* name);
-            InfraButton* get(size_t page, const char* name);
+                InfraButton* add(size_t page, const char* name);
+                void del(size_t page, const char* name);
+                InfraButton* get(size_t page, const char* name);
 
-            InfraButton* get(int id) { return buttons[id]; }
-            int totalCount() { return count; }
+                InfraButton* get(int id) { return buttons[id]; }
+                int totalCount() { return buttonCount; }
 
-            void sendListNames(BluetoothJsonResponse& target);
-            void sendButtonEdit(BluetoothJsonResponse& target, size_t page, const char* name);
+                void sendListNames(BluetoothJsonResponse& target);
+                void sendButtonEdit(BluetoothJsonResponse& target, size_t page, const char* name);
 
-            public:
-            int defBtnWidth = 95;
-            int defBtnHeight = 33;
-            int defSpacing = 3;
-            size_t pageCount = 1;
+                public:
+                int defBtnWidth = 95;
+                int defBtnHeight = 33;
+                int defSpacing = 3;
+                size_t pageCount = 1;
 
-            protected:
-            virtual bool onSave(JsonDocument& document);
-            virtual bool onLoad(JsonDocument& document);
-            virtual bool onDefault( void );
-            virtual size_t getJsonBufferSize() { return 48000; }
+                protected:
+                virtual bool onSave(JsonDocument& document);
+                virtual bool onLoad(JsonDocument& document);
+                virtual bool onDefault( void );
+                virtual size_t getJsonBufferSize() { return 48000; }
 
-            protected:
-            InfraButton *buttons[MAX_IR_BUTTONS];
-            int count = 0;
+                protected:
+                InfraButton** buttons = (InfraButton**)MALLOC(sizeof( InfraButton* ) * IR_BUTTONS_START);
+                size_t buttonCount = 0;
             };
         #else
             /**
