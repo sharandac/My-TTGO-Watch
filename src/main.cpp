6c97e62d435060f7da677e0145842070376caa4e
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2022-01-19 23:20:58 +0900

    Remove setup and declaration of removed apps

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,8 @@
 #include "hardware/powermgm.h"
 
 #include "app/calc/calc_app.h"
-#include "app/FindPhone/FindPhone.h"
-#include "app/example_app/example_app.h"
 #include "app/gps_status/gps_status.h"
 #include "app/kodi_remote/kodi_remote_app.h"
-#include "app/osmand/osmand_app.h"
 #include "app/powermeter/powermeter_app.h"
 #include "app/osmmap/osmmap_app.h"
 #include "app/mail/mail_app.h"
@@ -17,7 +14,6 @@
 #include "app/stopwatch/stopwatch_app.h"
 #include "app/wifimon/wifimon_app.h"
 #include "app/calendar/calendar.h"
-#include "app/weather/weather.h"
 #include "app/activity/activity.h"
 #include "app/fx_rates/fx_rates.h"
 #include "app/IRController/IRController.h"
@@ -50,7 +46,6 @@ void setup() {
      * apps here
      */
     osmmap_app_setup();
-    weather_app_setup();
     stopwatch_app_setup();
     alarm_clock_setup();
     activity_app_setup();
@@ -59,11 +54,8 @@ void setup() {
     gps_status_setup();
     IRController_setup();
     sailing_setup();
-    osmand_app_setup();
     fxrates_app_setup();
     powermeter_app_setup();
-    example_app_setup();
-    FindPhone_setup();
     wifimon_app_setup();
     calc_app_setup();
     kodi_remote_app_setup();
